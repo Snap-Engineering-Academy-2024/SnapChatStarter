fc1be451bba893f02b7cae17c11cbd6b8aca5721
Steven Nguyen <2017stevennguyen@gmail.com>
2023-08-03 12:07:34 -0700

    UPDATED YARN

--- src/screens/SpotlightScreen.js
+++ src/screens/SpotlightScreen.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button } from "react-native";
+import Modal from "react-native-modal";
 
 export default function SpotlightScreen() {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   return (
     <View>
       <Text>SpotlightScreen</Text>
+      <View style={{ flex: 0 }}>
+        <Button title="Show modal" onPress={toggleModal} />
+
+        <Modal isVisible={isModalVisible}>
+          <View style={{ flex: 0 }}>
+            <Text>This is a test of a Modal</Text>
+
+            <Button title="Hide modal" onPress={toggleModal} />
+          </View>
+        </Modal>
+      </View>
     </View>
   );
 }
