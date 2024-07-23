e04e311df9dc153dc9c08ab3786dec2e481d0b85
Cindy Andrade <161978639+cindyawho@users.noreply.github.com>
2024-07-23 10:53:54 -0700

    Temporary Bug Fix to Avatar Warning in Chat (#3)
    * Update .env.example to be .local file with my API keys
    
    * Update profile picture and comment out console logs
    
    * Temporary solution to avoid warning on user screen
    
    * Revert to default profile picture for ease of merging

--- src/utils/hooks/useAuthentication.js
+++ src/utils/hooks/useAuthentication.js
@@ -6,7 +6,7 @@ export function useAuthentication() {
 
   useEffect(() => {
     // Retrieve the current session
-    console.log('Supabase client:', supabase); // Debugging line
+    // console.log('Supabase client:', supabase); // Debugging line
 
     const fetchSession = async () => {
       const { data: { session } } = await supabase.auth.getSession();
