ae5801eb55060a393f5af17e326a752c4a663c51
Cindy Andrade <cindya3939@gmail.com>
2024-07-23 13:54:37 -0700

    Delete my own unecessary files

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,16 +30,9 @@ export default function Header({ title }) {
       </View>
       <Text style={styles.title}>{title}</Text>
       <View style={styles.headerRight}>
-        <Pressable
-            style={[styles.profile, styles.buttons]}
-            onPress={() => {
-              navigation.navigate("FriendAddingScreen");
-            }}
-          >
-          <View style={[styles.followers, styles.buttons]}>
-            <Followers />
-          </View>
-        </Pressable>
+        <View style={[styles.followers, styles.buttons]}>
+          <Followers />
+        </View>
         <View style={[styles.more, styles.buttons]}>
           <More />
         </View>
