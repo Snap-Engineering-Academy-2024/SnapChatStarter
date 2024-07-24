f3b94f9d01fe42a20f3d98b7fe020f5edb78bb7d
Sona Sargsyan <sargsyan.sona1.y@gmail.com>
2024-07-24 13:44:11 -0700

    added styles to event components

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -1,48 +1,130 @@
 import React from 'react'
 import { Card } from '@rneui/themed';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function EventScreen({ route, navigation }) {
     return (
-    <View>
+    <ScrollView>
+    <View style = {styles.Events}>
         <View style={styles.container}>
-          <Image
-              style={{width:"100%", aspectRatio:1}}
-              resizeMode="contain"
-              source={{ uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4" }}
-            />
-          <Card.Title style = {styles.title}>Sona's Birthday</Card.Title>
-          <View style = {styles.userInfo}>
-            <Image style = {styles.bitmojiUser} source = {{uri:"https://plus.unsplash.com/premium_photo-1664478383014-e8bc930be7c2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"}}/>
-            <Text style = {styles.username}>UserName Here</Text>
-          </View>
-          
+            <View style = {styles.friends}>
+                <Text style = {styles.friendsText}>3 friends going</Text>
+            </View>
+            <Image
+                style={{width:"100%", aspectRatio:1, borderRadius:20}}
+                resizeMode="contain"
+                source={{ uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4" }}
+                />
+            <Card.Title style = {styles.title}>Sona's Birthday</Card.Title>
+            <View style = {styles.userInfo}>
+                <Image style = {styles.bitmojiUser} source = {{uri:"https://plus.unsplash.com/premium_photo-1664478383014-e8bc930be7c2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"}}/>
+                <Text style = {styles.username}>username</Text>
+            </View>
+         </View>
+
+         <View style={styles.container}>
+            <View style = {styles.friends}>
+                <Text style = {styles.friendsText}>3 friends going</Text>
+            </View>
+            <Image
+                style={{width:"100%", aspectRatio:1, borderRadius:20}}
+                resizeMode="contain"
+                source={{ uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4" }}
+                />
+            <Card.Title style = {styles.title}>Sona's Birthday</Card.Title>
+            <View style = {styles.userInfo}>
+                <Image style = {styles.bitmojiUser} source = {{uri:"https://plus.unsplash.com/premium_photo-1664478383014-e8bc930be7c2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"}}/>
+                <Text style = {styles.username}>username</Text>
+            </View>
+         </View>
+
+         <View style={styles.container}>
+            <View style = {styles.friends}>
+                <Text style = {styles.friendsText}>3 friends going</Text>
+            </View>
+            <Image
+                style={{width:"100%", aspectRatio:1, borderRadius:20}}
+                resizeMode="contain"
+                source={{ uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4" }}
+                />
+            <Card.Title style = {styles.title}>Sona's Birthday</Card.Title>
+            <View style = {styles.userInfo}>
+                <Image style = {styles.bitmojiUser} source = {{uri:"https://plus.unsplash.com/premium_photo-1664478383014-e8bc930be7c2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"}}/>
+                <Text style = {styles.username}>username</Text>
+            </View>
          </View>
     </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
+    Events:{
+        padding:20,
+        width:"100%",
+        display:"flex",
+        gap:10,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+    
+    
+    },
     container:{
-        backgroundColor:"red",
+        width: "48%",
+        backgroundColor:"#E5E5E5",
         display:"flex",
         justifyContent:"space-between",
         // alignItems:"center",
-        padding:20,
-        gap:10
+        padding:10,
+        // gap:10,
+        borderRadius:20,
+        maxHeight:250,
+        margin:0,
     },
     bitmojiUser:{
-        width:50,
+        width:28,
         aspectRatio:1,
         borderRadius:1000,
+        margin:0,
+
 
     },
     title:{
         textAlign:"left",
-        backgroundColor:"grey",
+        marginTop:8,
+        marginBottom:5,
+        fontSize:15,
+        
     },
     userInfo:{
         display:"flex",
-        flexDirection:""
+        flexDirection:"row",
+        alignItems:"center",
+        gap:8,
+        margin:0
+    },
+    friends:{
+        position:"absolute",
+        top:15,
+        left:15,
+        zIndex:100,
+        backgroundColor:"pink",
+        margin:0,
+        borderRadius:20,
+        padding:10,
+
+    },
+    friendsText:{
+        fontWeight:"bold",
+        fontSize:10,
+    },
+    username:{
+        fontSize:11,
+        margin:0,
+        fontWeight:"bold",
+        color:"#575757"
+
     }
 })
