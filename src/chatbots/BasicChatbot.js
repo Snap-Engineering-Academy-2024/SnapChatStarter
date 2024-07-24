fe5bc82662f385d3de725f04eb8e617e1f925cc2
Jay Olivas <jayolivascox@gmail.com>
2024-07-23 17:50:33 -0700

    added my chatbot to the app

--- src/chatbots/BasicChatbot.js
+++ src/chatbots/BasicChatbot.js
@@ -2,38 +2,31 @@ import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, Platform } from "react-native";
-import defaultProfileImage from "../../assets/snapchat/defaultprofile12.png";
+import { getChat } from "../utils/getChatGpt";
 
-//Temporary Solution to avoid warning on user screen
-const error = console.error; 
-console.error = (...args) => { 
-  if (/defaultProps/.test(args[0])) return; error(...args); 
-};
+const prompt = [
+  {
+    role: "system",
+    content:
+      "Ask the user for their name on the first query, after that, ask the user what they are super knowledgeable about, if they say something like “I don’t know” or “nothing”, nicely tell them to ask one of their friends to play. If the user replies with their area of expertise, ask them if they want to play a trivia of that thing, if they say no, nicely ask them to ask their friends to play, if they say yes, give them 10 questions about that area of expertise. Unless their area of expertise isn't in your knowledge bank, if it isn't, simply tell the user nicely and nicely tell them to choose another area of expertise. As the user answers more questions correctly, give them harder questions. Keep track of their score and give it to them after the 10 questions. Ask one question at a time, and don't go onto the next question until the user answers.",
+  },
+];
 
 const CHATBOT_USER_OBJ = {
   _id: 2,
   name: "React Native Chatbot",
-  avatar: defaultProfileImage,
+  avatar: "https://loremflickr.com/140/140",
 };
 
 export default function BasicChatbot() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello, welcome to simple trivia! Say 'Yes' when you're ready to play!",
-        createdAt: new Date(),
-        user: CHATBOT_USER_OBJ,
-      },
-    ]);
+    fetchInitialMessage();
   }, []);
 
   const addNewMessage = (newMessages) => {
     setMessages((previousMessages) => {
-      // console.log("PREVIOUS MESSAGES:", previousMessages);
-      // console.log("NEW MESSAGE:", newMessages);
       return GiftedChat.append(previousMessages, newMessages);
     });
   };
@@ -49,17 +42,38 @@ export default function BasicChatbot() {
     ]);
   };
 
-  const respondToUser = (userMessages) => {
-    // console.log("User message text:", userMessages[0].text);
+  async function getChatResponse(allMessages){
+    //console.log(allMessages);
+    const response = await getChat(allMessages.reverse());
+    const message = response.choices[0].message;
+    const content = message.content;
+    addBotMessage(content);
+  }
 
-    // Simple chatbot logic (aka Checkpoint 2 onwards) here!
-
-    addBotMessage("Welcome to a recreated version of SnapChat");
+  const respondToUser = (newMessages) => {
+    let allMessages = [newMessages[0], ...messages];
+    allMessages = allMessages.map((element) => {
+      if (element.user.name === "React Native Chatbot" ){
+        return {role: "assistant", content: element.text}
+      }
+      else{
+        return {role: "user", content: element.text}
+      }
+    })
+    allMessages.push(prompt[0]);
+    getChatResponse(allMessages);
   };
 
   const onSend = useCallback((messages = []) => {
     addNewMessage(messages);
   }, []);
+
+  async function fetchInitialMessage() {
+    const response = await getChat(prompt);
+    const message = response.choices[0].message;
+    const content = message.content;
+    addBotMessage(content);
+  }
 
   return (
     <GiftedChat
@@ -70,7 +84,7 @@ export default function BasicChatbot() {
       }}
       user={{
         _id: 1,
-        name: "Alexis",
+        name: "Jay",
       }}
       renderUsernameOnMessage={true}
     />
