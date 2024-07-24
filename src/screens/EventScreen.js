e179970649a450bda1c4702059c8fa1e520ef6d1
Sona Sargsyan <sargsyan.sona1.y@gmail.com>
2024-07-24 11:39:36 -0700

    adding events

--- src/screens/EventScreen.js
+++ src/screens/EventScreen.js
@@ -12,7 +12,11 @@ export default function EventScreen({ route, navigation }) {
               source={{ uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4" }}
             />
           <Card.Title style = {styles.title}>Sona's Birthday</Card.Title>
-          <Text style = {styles.username}>UserName Here</Text>
+          <View style = {styles.userInfo}>
+            <Image style = {styles.bitmojiUser} source = {{uri:"https://plus.unsplash.com/premium_photo-1664478383014-e8bc930be7c2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"}}/>
+            <Text style = {styles.username}>UserName Here</Text>
+          </View>
+          
          </View>
     </View>
   )
@@ -27,8 +31,17 @@ const styles = StyleSheet.create({
         padding:20,
         gap:10
     },
+    bitmojiUser:{
+        width:50,
+        aspectRatio:1,
+        borderRadius:1000,
+
+    },
     title:{
         textAlign:"left",
-        backgroundColor:"blue",
+        backgroundColor:"grey",
+    },
+    userInfo:{
+        display:"flex"
     }
 })
