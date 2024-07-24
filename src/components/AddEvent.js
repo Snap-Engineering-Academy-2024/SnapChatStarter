c2cf504dc44e1b7e781389bc8a38f5749053039f
Sona Sargsyan <sargsyan.sona1.y@gmail.com>
2024-07-24 14:35:30 -0700

    input fields added

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,18 +1,49 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TextInput, Image, Button, TouchableOpacity } from "react-native";
 
 export default function AddEvent() {
   return (
     <View style = {styles.userInfo}>
-       <Text>Component</Text> 
+       <Text style = {styles.eventText}>CREATE EVENT</Text> 
+       <TextInput style = {styles.inputFields} placeholder = "Title"></TextInput>
+       <TextInput style = {styles.descriptionField} placeholder = "Description"></TextInput>
+       <TextInput style = {styles.inputFields} placeholder = "Time"></TextInput>
+       <TextInput style = {styles.inputFields} placeholder = "Location"></TextInput>
+
     </View>
   )
 }
 const styles = StyleSheet.create({
     userInfo:{
-        backgroundColor:"red",
-        height:100,
-        width:100,
-        position:"absolute"
+        backgroundColor:"white",
+        borderWidth:1,
+        borderColor:"yellow",
+        width:"80%",
+        aspectRatio:1,
+        position:"absolute",
+        alignSelf:"center",
+        top:"20%",
+        borderRadius:20,
+        padding:20,
+
+
+    },
+    eventText:{
+        textAlign:"center",
+        fontSize:23,
+        fontWeight:"bold",
+    },
+    inputFields:{
+        marginTop:10,
+        backgroundColor:"#F0F0F0",
+        padding:5,
+        borderRadius:5,
+    },
+    descriptionField:{
+        marginTop:10,
+        backgroundColor:"#F0F0F0",
+        padding:5,
+        borderRadius:5,
+        paddingBottom:30,
     }
 })
