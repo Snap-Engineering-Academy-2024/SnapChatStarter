9efc6c03f04d630bb3d516f99c1f776fd0a5978f
Sedrick Moore II <sedrickmoore@2gmail.com>
2024-07-25 12:32:22 -0700

    merged both Bee and I

--- src/screens/AstrologyScreen.js
+++ src/screens/AstrologyScreen.js
@@ -10,6 +10,9 @@ const requestOptions = {
 export default function AstrologyScreen() {
   const [astrology, setAstrology] = useState(null);
   const [horoscope, setHoroscope] = useState();
+  const [signAbout, setSignAbout] = useState();
+  const [signElement, setSignElement] = useState();
+  const [signCompatibility, setSignCompatibility] = useState();
   const userSign = findAstrologySign();
 
   useEffect(() => {
@@ -20,6 +23,7 @@ export default function AstrologyScreen() {
   useEffect(() => {
     if (astrology === null) return;
     getHoroscope();
+    getSignDescription();
   }),
     [astrology];
 
@@ -33,11 +37,39 @@ export default function AstrologyScreen() {
       .catch((error) => console.error(error));
   }
 
+  async function getSignDescription() {
+    const myHeaders = new Headers();
+    myHeaders.append("x-rapidapi-host", "horoscope-astrology.p.rapidapi.com");
+    myHeaders.append("x-rapidapi-key", process.env.EXPO_PUBLIC_X_RAPIDAPI_KEY);
+
+    const url = `https://horoscope-astrology.p.rapidapi.com/sign?s=${astrology.toLowerCase()}`;
+    const options = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((result) => {
+        setSignAbout(result.about);
+        setSignElement(result.element);
+        setSignCompatibility(result.compatibility);
+      })
+      .catch((error) => console.error("Error message:", error));
+  }
+
   return (
     <View style={styles.page}>
       <View style={{ alignItems: "center", paddingTop: 25 }}>
         <Text style={styles.contentTitles}>Your Sign</Text>
         <Text style={styles.sign}>{astrology}</Text>
+        <Text style={styles.contentTitles}>Element</Text>
+        <Text style={styles.contentText}>{signElement}</Text>
+        <Text style={styles.contentTitles}>About</Text>
+        <Text style={styles.contentText}>{signAbout}</Text>
+        <Text style={styles.contentTitles}>Compatible Signs</Text>
+        <Text style={styles.contentText}>{signCompatibility}</Text>
         <Text style={styles.contentTitles}>Today's Horoscope</Text>
         <Text style={styles.contentText}>{horoscope}</Text>
       </View>
@@ -55,20 +87,20 @@ const styles = StyleSheet.create({
     fontFamily: "Papyrus",
     fontWeight: "bold",
     fontSize: 30,
-    color: "gold"
+    color: "gold",
   },
   sign: {
     paddingTop: 30,
     paddingBottom: 30,
     fontFamily: "Papyrus",
     fontSize: 25,
-    color: "lightyellow"
+    color: "lightyellow",
   },
   contentText: {
     paddingTop: 30,
     paddingBottom: 30,
     fontFamily: "Papyrus",
     fontSize: 18,
-    color: "lightyellow"
+    color: "lightyellow",
   },
 });
