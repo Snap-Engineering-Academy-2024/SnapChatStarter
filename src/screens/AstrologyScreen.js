6f98cd34a8dc0c01427a5743833491c3eb6d798c
Sedrick Moore II <sedrickmoore@2gmail.com>
2024-07-25 11:47:38 -0700

    some styling. did not fix null calls.

--- src/screens/AstrologyScreen.js
+++ src/screens/AstrologyScreen.js
@@ -1,4 +1,4 @@
-import { Image, Text, View, Button } from "react-native";
+import { Image, Text, View, StyleSheet } from "react-native";
 import { supabase } from "../utils/hooks/supabase";
 import { useState, useEffect } from "react";
 import { findAstrologySign } from "../utils/hooks/findAstrologySign";
@@ -11,17 +11,19 @@ const requestOptions = {
 export default function AstrologyScreen() {
   const [astrology, setAstrology] = useState(null);
   const [horoscope, setHoroscope] = useState();
-  const userSign = findAstrologySign()
+  const userSign = findAstrologySign();
 
-  useEffect(()=>{
-    setAstrology(userSign.sign)
-  }),[]
+  useEffect(() => {
+    setAstrology(userSign.sign);
+  }),
+    [];
 
-  useEffect(()=>{
-    if(astrology === null) return
+  useEffect(() => {
+    if (astrology === null) return;
     getHoroscope();
-    console.log(horoscope)
-  }),[astrology]
+    console.log(horoscope);
+  }),
+    [astrology];
 
   async function getHoroscope() {
     fetch(
@@ -34,13 +36,41 @@ export default function AstrologyScreen() {
   }
 
   return (
-    <View style={{ alignItems: "center", paddingTop: 50 }}>
-      <Text style={{ justifyContents: "center", paddingTop: 50 }}>
-        {astrology}
-      </Text>
-      <Text style={{ justifyContents: "center", paddingTop: 50 }}>
-        {horoscope}
-      </Text>
+    <View style={styles.page}>
+      <View style={{ alignItems: "center", paddingTop: 25 }}>
+        <Text style={styles.contentTitles}>Your Sign</Text>
+        <Text style={styles.sign}>{astrology}</Text>
+        <Text style={styles.contentTitles}>Today's Horoscope</Text>
+        <Text style={styles.contentText}>{horoscope}</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: "purple",
+  },
+  contentTitles: {
+    justifyContents: "center",
+    fontFamily: "Papyrus",
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "gold"
+  },
+  sign: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    fontFamily: "Papyrus",
+    fontSize: 25,
+    color: "lightyellow"
+  },
+  contentText: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    fontFamily: "Papyrus",
+    fontSize: 18,
+    color: "lightyellow"
+  },
+});
