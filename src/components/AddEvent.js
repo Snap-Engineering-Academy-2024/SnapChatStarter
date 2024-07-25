d391341bad92a26320b5beda1b6902557b621619
Christian <josephcgarcia1001@gmail.com>
2024-07-24 18:03:57 -0700

    Yeah i can send to superbase. Wdym you can't? its easy.

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -9,19 +9,48 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { Dialog } from "@rneui/themed";
-import { useAuthentication } from "../utils/hooks/useAuthentication";
+import { supabase } from "../utils/hooks/supabase";
 
-
-const {user} = useAuthentication();
-
-useEffect (() => {
-  //async function 
-}
- )
 
 export default function AddEvent({ isVisible, onClose }) {
+
+  const insertData = async () => {
+    const eventData = {
+      id: "20",
+      title: "Sona's Birthday",
+      description: "Come celebrate the 2nd coolest birthday ever!",
+      location: "Santa Monica, CA",
+      time: "2024-12-07 03:30:00+00",
+      imageURL:
+        "https://sdk.bitmoji.com/render/panel/20087589-103221902646_4-s5-v1.png?transparent=1&palette=1&scale=2",
+      host: '{"userName":"","imageURL":"https://sdk.bitmoji.com/render/panel/20048676-103221902646_4-s5-v1.png?transparent=1&palette=1&scale=1"}',
+      attending:
+        '[{"userName":"","imageURL":""},{"userName":"","imageURL":""},{"userName":"","imageURL":""}]',
+      private: "true",
+      created_at: new Date().toISOString(),
+    };
+
+    try {
+      const { data, error } = await supabase
+        .from("event_tbl") // Replace 'event_tbl' with your actual table name
+        .insert([eventData]); // Insert the event data
+
+      if (error) {
+        console.error("Error inserting data:", error);
+      } else {
+        console.log("Data inserted:", data);
+      }
+    } catch (error) {
+      console.error("Unexpected error:", error);
+    }
+  };
+
   return (
-    <Dialog overlayStyle= {styles.DialogueBox} isVisible={isVisible} onBackdropPress={onClose}>
+    <Dialog
+      overlayStyle={styles.DialogueBox}
+      isVisible={isVisible}
+      onBackdropPress={onClose}
+    >
       <Text style={styles.eventText}>Event Details</Text>
       <TextInput style={styles.inputFields} placeholder="Title"></TextInput>
       <TextInput
@@ -30,9 +59,9 @@ export default function AddEvent({ isVisible, onClose }) {
       ></TextInput>
       <TextInput style={styles.inputFields} placeholder="Time"></TextInput>
       <TextInput style={styles.inputFields} placeholder="Location"></TextInput>
-      <Button title = "Upload pic"> </Button>
+      <Button title="Upload pic"> </Button>
       <Dialog.Actions>
-        <Button title="Create Event" />
+        <Button title="Create Event" onPress={insertData} />
         <Button title="Close" onPress={onClose} />
       </Dialog.Actions>
     </Dialog>
@@ -51,9 +80,9 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 20,
   },
-  DialogueBox:{
+  DialogueBox: {
     height: "60%",
-    borderRadius:20,
+    borderRadius: 20,
     // backgroundColor :"red"
   },
   eventText: {
