ee8fdc68456dd57a41235f13920f2b1ad657adfa
Allison Hua <allisonhua89@gmail.com>
2024-07-25 14:50:41 -0700

    fixed merge conflict

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
 import { Image , Text, View, ActivityIndicator } from "react-native";
 import { useAuthentication } from "../utils/hooks/useAuthentication";
@@ -32,37 +31,4 @@ export default function ProfileScreen(){
                 {user.user_metadata.email.slice(0, user.user_metadata.email.indexOf("@"))}
             </Text>
         </View>
-    );       
-=======
-import { Image , Text, View, Button} from "react-native";
-import { supabase } from '../utils/hooks/supabase';
-
-const handleSignOut = async () => {
-    try {
-      const { error } = await supabase.auth.signOut();
-      if (error) {
-        console.error("Error signing out:", error.message);
-      } else {
-        // Handle successful sign out (e.g., redirect to login screen)
-      }
-    } catch (error) {
-      console.error("Unexpected error:", error);
-    }
-  };
-
-export default function ProfileScreen(){
-return(
-    <View styles={{alignItems:"center"}}>
-    <Image 
-    source={{uri:"https://loremflickr.com/cache/resized/65535_52294428543_2d04971c12_n_320_240_nofilter.jpg"}}
-    style={{width: 400, height: 400, borderRadius: 400/ 2}} 
-    />
-    <Text 
-        styles={{justifyContents:"center"}}>
-        User Name Would Go Here
-    </Text>
-    <Button onPress={handleSignOut} title="Log Out" />
-    </View>
-);
->>>>>>> 91cc52acb7addbb894744e82d1070b8e9066647d
-};
+    );  
