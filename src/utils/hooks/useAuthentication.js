98e325db128e5acfcc7f734b3ef478c2a672323f
Julissa Hernandez Romero <jhern430@calpoly.edu>
2024-07-31 14:11:20 -0700

    fixed useAuthentication random main

--- src/utils/hooks/useAuthentication.js
+++ src/utils/hooks/useAuthentication.js
@@ -6,7 +6,7 @@ export function useAuthentication() {
 
   useEffect(() => {
     // Retrieve the current session
-  main
+    // console.log('Supabase client:', supabase); // Debugging line
 
     const fetchSession = async () => {
       const { data: { session } } = await supabase.auth.getSession();
