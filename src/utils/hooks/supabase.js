745dfda9e866676f1c6f7950fb6d3d2bca531a5b
Christian <josephcgarcia1001@gmail.com>
2024-07-31 14:06:53 -0700

    Fixed env

--- src/utils/hooks/supabase.js
+++ src/utils/hooks/supabase.js
@@ -8,9 +8,9 @@ import { createClient } from '@supabase/supabase-js';
 // console.log('Supabase URL:', process.env.EXPO_PUBLIC_SUPABASE_URL);
 // console.log('Supabase Anon Key:', process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
 
-export const supabase = createClient(
-    process.env.EXPO_PUBLIC_SUPABASE_URL || "https://nxolmkncetbvbwuodlpa.supabase.co",
-    process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54b2xta25jZXRidmJ3dW9kbHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEzMzczNzAsImV4cCI6MjAzNjkxMzM3MH0.peRJDosyL2hjjlM__wtzWfTqypdMLvm83Ky9i441h-A",
+export const supabase = createClient( 
+    process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+    process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
   {
     auth: {
       storage: AsyncStorage,
