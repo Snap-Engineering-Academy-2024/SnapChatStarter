f7cb85f1036b64d2a9d173a19e9edb6ea9d4b403
ryanhongnguyen <139264626+ryanhongnguyen@users.noreply.github.com>
2024-08-01 10:17:43 -0700

    Fix multiple screen bug

--- src/navigation/UserStack.js
+++ src/navigation/UserStack.js
@@ -35,11 +35,6 @@ export default function () {
           options={{ headerShown: false}}
         />
         <Stack.Screen
-          name="DiscoverCard"
-          component={DiscoverCard}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
           name="MemoryScreen"
           component={MemoryScreen}
           options={{ headerShown: true }}
