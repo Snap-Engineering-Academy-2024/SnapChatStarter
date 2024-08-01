b69f421953860295d36dc17d563fcebc5b395ccd
Sedrick Moore II <sedrickmoore@2gmail.com>
2024-08-01 16:30:55 -0700

    All screens are connected

--- src/screens/SnapTogetherScreen.js
+++ src/screens/SnapTogetherScreen.js
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet, SafeAreaView, Button } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 // import Ionicons from "react-native-vector-icons/Ionicons";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { supabase } from "../utils/hooks/supabase"; // Import Supabase client
+import { useNavigation } from "@react-navigation/native";
 
 import Header from "../components/Header";
 import { CHATBOTS } from "./ConversationScreen";
 
 export default function SnapTogetherScreen() {
+  const navigation = useNavigation();
 
   return (
     <SafeAreaView>
       <Text>
         "Hi I'm the Snap Together Screen"
       </Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("Section");
+        }}
+        title={"Sections"}
+        color="brown"
+        accessibilityLabel="Section redirect button"
+      />
     </SafeAreaView>
   );
 }
