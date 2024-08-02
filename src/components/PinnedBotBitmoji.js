5a9f14329759ed8a2d6c704baa3e7f2b91ffbc36
masielmartinez <msl.mrtnz.007@gmail.com>
2024-08-01 21:57:37 -0700

    Create PinnedBotBitmoji.js

---
+++ src/components/PinnedBotBitmoji.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { fontHeader } from "../../assets/themes/font";
+import { colors } from "../../assets/themes/colors";
+// import StoriesBitmoji from "../components/StoriesBitmoji";
+
+import Header from "../components/Header";
+
+export default function PinnedBotBitmoji({name="defualtName"}) {
+  return (
+    <View style={styles.myBitmoji}>
+      <Image
+        style={styles.bitmojiImage}
+        source={require("../../assets/snapchat/personalBitmoji.png")}
+      />
+      <View style={styles.bitmojiTextContainer}>
+        <Text style={styles.bitmojiText}>{name}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  myBitmoji: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bitmojiImage: {
+    width: 60,
+    height: 60,
+  },
+  bitmojiTextContainer: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 4,
+  },
+  bitmojiText: {
+    alignSelf: "center",
+    fontSize: 12,
+    fontWeight: "700",
+  },
+  usernameText: {
+    fontSize: 8,
+    fontWeight: "700",
+    opacity: 0.5,
+  },
+  Friends: {
+    textAlign: "left",
+    paddingLeft: 20,
+    paddingBottom: 20,
+    color: colors.primary,
+    fontSize: fontHeader.fontSize,
+    fontFamily: fontHeader.fontFamily,
+    fontWeight: fontHeader.fontWeight,
+  },
+});
