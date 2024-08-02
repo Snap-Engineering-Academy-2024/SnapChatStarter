6d3754396d1659aa6f58e2589bc2e1c7b3d61791
Jay Olivas <jayolivascox@gmail.com>
2024-08-02 11:55:37 -0700

    Added a new component "DraggableButtons.js" for cleanliness and made the draggable list include the buttons we use in ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,10 @@
-import { Image, Text, View, Button, StyleSheet, Pressable} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Image, Text, View, StyleSheet, Button } from "react-native";
 import { supabase } from "../utils/hooks/supabase";
-import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { findAstrologySign } from "../utils/hooks/findAstrologySign";
 import { useAuthentication } from "../utils/hooks/useAuthentication";
+import DraggableButtonList from "../components/DraggableButtons"; 
 
 const handleSignOut = async () => {
   try {
@@ -26,61 +27,38 @@ export default function ProfileScreen() {
 
   useEffect(() => {
     setAstrology(userSign.sign);
-  }),
-    [];
+  }, [userSign.sign]);
+
+  const onPressHandlers = {
+    Astrology: () => navigation.navigate("Astrology"),
+    "Snap Together Badge": () => navigation.navigate("SnapTogether"),
+    "Snap Together": () => navigation.navigate("SnapTogether"),
+    "Log Out": handleSignOut,
+    Settings: () => navigation.navigate("Settings"),
+  };
 
   return (
-    <View style={{ alignItems: "center" }}>
+    <View style={styles.container}>
       <Image
         source={{ uri: "https://i.imgur.com/FxsJ3xy.jpg" }}
-        style={{ width: 150, height: 150, borderRadius: 150 / 2 }}
+        style={styles.avatar}
       />
-      <Text
-        style={{
-          justifyContents: "center",
-          textAlign: "center",
-        }}
-      >
+      <Text style={styles.emailText}>
         {user &&
           user.user_metadata &&
           user.user_metadata.email.slice(
             0,
-            user.user_metadata.email.indexOf("@"), // gets part before @ of email address, should use profile username instead
+            user.user_metadata.email.indexOf("@")
           )}
       </Text>
+      <DraggableButtonList onPressHandlers={onPressHandlers} />
+      <View>
+        <Button onPress={handleSignOut} title="Log Out" />
       <Button
-        onPress={() => {
-          navigation.navigate("Astrology");
-        }}
-        title={astrology}
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
+        onPress={() => navigation.navigate("Settings")}
+        title="Settings"
       />
-      <Button
-        onPress={() => {
-          navigation.navigate("SnapTogether");
-        }}
-        title={"Snap Together Badge"}
-        color="brown"
-        accessibilityLabel="The Badge for Snap Together"
-      />
-      <Button
-        onPress={() => {
-          navigation.navigate("SnapTogether");
-        }}
-        title={"Snap Together"}
-        color="brown"
-        accessibilityLabel="Snap Together redirect button"
-      />
-      <Button onPress={handleSignOut} title="Log Out" />
-      <Pressable>
-        <Button
-          onPress={() => {
-            navigation.navigate("Settings", {});
-          }}
-          title="Settings"
-        />
-      </Pressable>
+      </View>
     </View>
   );
 }
@@ -88,13 +66,33 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
+    flex: 1,
     flexDirection: "column",
     alignItems: "center",
+    justifyContent: "center",
+  },
+  draggableContainer: {
+    height: 100,
+    marginBottom: 20,
   },
   avatar: {
     width: 150,
     height: 150,
-    borderRadius: 150 / 2,
-    alignItems: "center",
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+  emailText: {
+    justifyContents: "center",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  button: {
+    padding: 20,
+    marginHorizontal: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
