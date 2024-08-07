d86dde395c0d4f6856b0c6c3836d2197ceadcb53
Areli Arana <aranaareli1@gmail.com>
2024-08-07 15:34:14 -0700

    Added more styling to Location Details

--- src/screens/LocationDetails.js
+++ src/screens/LocationDetails.js
@@ -5,35 +5,20 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import { inline } from 'react-native-web/dist/cjs/exports/StyleSheet/compiler';
 import { fontHeader } from '../../assets/themes/font';
 
-const { height } = Dimensions.get('window');
+const { height, width} = Dimensions.get('window');
 
-export default function LocationDetails({ place, onClose }) {
+export default function LocationDetails({ place, onClose,  }) {
 
   let price = "$";
-  let open;
-
-  if (place.opening_hours.open_now) {
-    open = "Open Now";
-  } else {
-    open = "Closed"
-  }
-
   const Circle = () => {
     return <View style={styles.circle} />;
   };
 
-  const Star = () =>  {
-    return (
-    <View style={styles.iconContainer}>
-      <Ionicons name="star-outline" size={20} color="black" />
-    </View>)
-  }
   const Rectangle = () => {
     return <View style={styles.rectangle} />;
   };
-  //console.log(JSON.stringify(place, null, 4));
+  console.log(JSON.stringify(place, null, 4));
   // console.log("TYPES", place.types[0])
-
   return (
     //<View style={styles.modalOverlay}>
       <ScrollView style={styles.container}>
@@ -50,13 +35,10 @@ export default function LocationDetails({ place, onClose }) {
             </View>
                 
             <Text style={{ textTransform: 'capitalize' }}>{place.types[0]} - ${price.repeat(place.price_level)}</Text>
-            <Text>{open} - The Rocks</Text>
-            <View style={styles.iconContainer}>
-              {/* <View style={styles.inline}> */}
+            <Text>{(typeof place.opening_hours !='undefined')? (place.opening_hours.open_now? 'Open Now - ' : 'Closed - '):''}The Rocks</Text>
+            {/* <View style={styles.iconContainer}>
                 <Ionicons name="star-outline" size={20} color="black" />
-              {/* <Ionicons name="star-outline" size={20} color="black" /> */}
-              {/* </View> */}
-            </View>
+            </View> */}
           </View>
          </View>
 
@@ -89,24 +71,106 @@ export default function LocationDetails({ place, onClose }) {
               <View style={styles.iconContainer}>
                 <Ionicons name="caret-forward-outline" size={20} color="white" />
               </View>
-              <Text style={styles.buttonText}></Text>
           </TouchableOpacity>  
           </View>
           
        
         {/* END OF HEADER SECTION */}
-        <View style={styles.donateButton}>
-            <Text style={styles.donateText}>Looking to Help? Donate now!</Text>
-            <Text>safeplaceforyouth.org</Text>
-          </View>
+        <TouchableOpacity>
+          <View style={styles.donateButton}>
+            <View style={styles.donateTextSection}>
+              <Text style={styles.donateText}>Looking to Help? Donate now!</Text>
+              <Text style={{color:"grey"}}>safeplaceforyouth.org</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Ionicons name="chevron-forward-outline" size={20} color="grey"/>
+            </View>
+        </View>
+        </TouchableOpacity>
+        
         {/* SPOTLIGHT SECTION */}
-        <View style={styles.inline}>
+        <View style={styles.spotlight}>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
           <Rectangle></Rectangle>
         </View>
+
+{
+  typeof place.opening_hours !='undefined' && 
+      <TouchableOpacity>
+          <View style={styles.scheduleButton}>
+            <View style={styles.iconContainer}>
+                <Ionicons name="time-outline" size={20} color="black" />
+              </View>
+            <View style={{width: width / 1.6,}}>
+              {
+                place.opening_hours.open_now && 
+                <Text style={{color: "green", fontSize: 15}}>Open Now</Text>
+              }
+              {
+                !place.opening_hours.open_now &&
+                <Text style={{color: "red", fontSize: 15}}>Closed</Text>
+              }
+              <Text style={{color: "grey"}}>8:00 AM - 2:00 PM</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Ionicons name="chevron-down-outline" size={20} color="grey"/>
+            </View>
+        </View>
+        </TouchableOpacity>
+}
+        <View style={styles.footerButtonSection}>
+            <TouchableOpacity>
+              <View style={styles.footerButton}>
+                <View style={styles.iconContainer}>
+                  <Ionicons name="location-outline" size={20} color="black" />
+                </View>
+                <Text style={{width: width / 1.5,}}>Address</Text>
+                <View style={styles.iconContainer}>
+              <Ionicons name="chevron-forward-outline" size={20} color="grey"/>
+            </View>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <View style={styles.footerButton}>
+                <View style={styles.iconContainer}>
+                  <Ionicons name="call-outline" size={20} color="black" />
+                </View>
+                <Text style={{width: width / 1.5,}}>Call</Text>
+                <View style={styles.iconContainer}>
+              <Ionicons name="chevron-forward-outline" size={20} color="grey"/>
+            </View>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <View style={styles.footerButton}>
+                <View style={styles.iconContainer}>
+                  <Ionicons name="earth-outline" size={20} color="black" />
+                </View>
+                <Text style={{width: width / 1.5,}}>Visit Website</Text>
+                <View style={styles.iconContainer}>
+              <Ionicons name="chevron-forward-outline" size={20} color="grey"/>
+            </View>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <View style={styles.footerButton}>
+                <View style={styles.iconContainer}>
+                  <Ionicons name="flag-outline" size={20} color="black" />
+                </View>
+                <Text style={{width: width / 1.5,}}>Report an issue</Text>
+                <View style={styles.iconContainer}>
+              <Ionicons name="chevron-forward-outline" size={20} color="grey"/>
+            </View>
+              </View>
+            </TouchableOpacity>
+        </View>
+
+        <View style={{height: height/8}} ></View> 
     </ScrollView>
-    //</View>    
   );
 }
 
@@ -124,26 +188,29 @@ const styles = StyleSheet.create({
     //justifyContent: 'flex-end',
   },
   title: {
-    fontSize: 25,
+    fontSize: 20,
     fontWeight: 'bold',
     // marginBottom: 10,
+    flexWrap: "wrap"
   },
   details: {
     fontSize: 16,
     marginBottom: 10,
   },
   circle: {
-    width: 75,
-    height: 75,
+    width: width/6,
+    height: width/6,
     borderRadius: 100 / 2,
     backgroundColor: "grey",
   },
   header: {
     flexDirection: "row",
-    flexWrap: "wrap",
+    // flexWrap: "wrap",
     columnGap: 20,
     justifyContent: "flex-start",
-    marginBottom: 10
+    marginBottom: 10,
+    width: width/2,
+    alignSelf: "stretch",
   }, 
   closeButton: {
     alignSelf: "center",
@@ -171,7 +238,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginRight: 6,
-    flexDirection: "row"
+    flexDirection: "row",
+    alignSelf: "center"
   },
   rectangle: {
     width: 70 * 2,
@@ -202,8 +270,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   donateButton: {
-    alignSelf: "stretch",
-    width: 350,
+    //alignSelf: "stretch",
+    width: width/1.1,
     backgroundColor: "white",
     borderRadius: 5,
     shadowColor: "black",
@@ -211,12 +279,15 @@ const styles = StyleSheet.create({
     shadowOpacity: .1, 
     shadowOffset: 10,
     padding: 15,
-    marginBottom: 15
+    marginBottom: 15,
+    flexDirection: "row",
+    columnGap: 10
   },
   donateText: {
     fontFamily: fontHeader.fontFamily,
     fontSize: 15,
-    color: "#0FADFF"
+    color: "#0FADFF",
+    fontWeight: "600"
   },
   shareButton: {
     backgroundColor: "#0FADFF",
@@ -226,6 +297,44 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     width: 110,
-    justifyContent: "center"
+    justifyContent: "center", 
+  },
+  donateTextSection: {
+    width: width / 1.4,
+  },
+  spotlight: {
+    flexDirection: "row",
+    columnGap: 25,
+    marginBottom: 15
+  },
+  scheduleButton: {
+    width: width/1.1,
+    backgroundColor: "white",
+    borderRadius: 5,
+    shadowColor: "black",
+    shadowRadius: 8,
+    shadowOpacity: .1, 
+    shadowOffset: 10,
+    padding: 15,
+    marginBottom: 15,
+    flexDirection: "row",
+    columnGap: 10
+  },
+  footerButton: {
+    width: width/1.1,
+    backgroundColor: "white",
+    padding: 15,
+    flexDirection: "row",
+    borderBottomColor: "light grey",
+    borderBottomWidth: .2,
+    //columnGap: 10
+  },
+  footerButtonSection: {
+    shadowColor: "black",
+    shadowRadius: 8,
+    shadowOpacity: .1, 
+    shadowOffset: 10,
+    borderRadius: 5,
+    columnGap: 10
   }
 });
