fa097450a998f90806430cfd239bb4bc2db831c0
Sedrick Moore II <sedrickmoore@2gmail.com>
2024-08-06 23:31:12 -0700

    Magically fixed the badges, added a "new" badge for the SnapTogether button when the user has not joined.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -61,6 +61,7 @@ export default function ProfileScreen() {
               onPressHandlers={sectionOnPressHandlers}
               badgeOnPressHandlers={badgeOnPressHandlers}
               astrology={astrology}
+              userJoinStatus={userJoinStatus}
             />
           </View>
         </BottomSheet>
